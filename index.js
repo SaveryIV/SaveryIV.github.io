194ed1a6ff75325283ab51d51cce2dd97e566977
SaveryIV <soriaagustin8@gmail.com>
2023-06-19 07:01:52 -0300

    Adding open and close functionalities

--- index.js
+++ index.js
@@ -1,13 +1,99 @@
-let $title = document.querySelector(".title");
+const $body = document.querySelector('.body');
+const $hamburger = document.querySelector('#hamburger');
+const $navbar = document.querySelector('.nav-links');
+const $closeMenuButton = document.querySelector('.nav-links img');
+const $linksMenu = document.querySelectorAll('.nav-links li a');
+const $modal = document.querySelector('.modal');
+const $modalTitle = document.querySelector('#modal-title');
+const $pModal = document.querySelector('#p-modal');
+const $imgModal = document.querySelector('#img-modal');
+const $liModal = document.querySelectorAll('.li-modal');
+const $openModal = document.querySelectorAll('.button-project');
+const $closeModal = document.querySelector('.close-button');
+const frstImagePath = '/resources/work-section/masks/Snapshoot PortfolioTonic-mask.png';
+const scndImagePath = '/resources/work-section/masks/Snapshoot PortfolioMulti-Post-mask.png';
+const thrdImagePath = '/resources/work-section/masks/Snapshoot PortfolioTonic2-mask.png';
+const frthImagePath = '/resources/work-section/masks/Snapshoot PortfolioMulti-Post2-mask.png';
+let scrollEnabled = true;
+const projectsInfo = [{
+  name: 'Tonic',
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  technologies: ['HTML', 'CSS', 'Javascript'],
+  image: frstImagePath,
+}, {
+  name: 'Multi Post Stories',
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  technologies: ['HTML', 'CSS', 'Javascript'],
+  image: scndImagePath,
+}, {
+  name: 'Tonic',
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  technologies: ['HTML', 'CSS', 'Javascript'],
+  image: thrdImagePath,
+}, {
+  name: 'Multi-Post Stories',
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  technologies: ['HTML', 'CSS', 'Javascript'],
+  image: frthImagePath,
+}];
 
-let div = document.createElement('div');
-div.id = 'content';
-div.className = 'note';
+function selectProject(buttonIndex) {
+  const project = projectsInfo[buttonIndex];
+  $pModal.textContent = project.description;
+  $imgModal.setAttribute('src', project.image);
+  $liModal.forEach((element, index) => {
+    element.textContent = project.technologies[index];
+  });
+  $modalTitle.textContent = project.name;
+}
 
-// create a new heading and add it to the div
-let h2 = document.createElement('h2');
-h2.textContent = 'Add h2 element to the div';
-div.appendChild(h2);
+function handleScroll() {
+  if (!scrollEnabled) {
+    window.scrollTo(0, 0);
+  }
+}
 
-// add div to the document
-document.body.appendChild(div);
+function popupScrollLock() {
+  $body.classList.toggle('popup-open');
+}
+
+function displayMenu() {
+  scrollEnabled = false;
+  $navbar.style.display = 'flex';
+}
+
+function closeMenuHandler() {
+  scrollEnabled = true;
+  $navbar.style.display = 'none';
+}
+
+$openModal.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    selectProject(index);
+    popupScrollLock();
+    $modal.showModal();
+  });
+});
+
+$hamburger.addEventListener('click', () => {
+  displayMenu();
+});
+
+$closeMenuButton.addEventListener('click', () => {
+  closeMenuHandler();
+});
+
+window.addEventListener('scroll', () => {
+  handleScroll();
+});
+
+$linksMenu.forEach((e) => {
+  e.addEventListener('click', () => {
+    closeMenuHandler();
+  });
+});
+
+$closeModal.addEventListener('click', () => {
+  popupScrollLock();
+  $modal.close();
+});
